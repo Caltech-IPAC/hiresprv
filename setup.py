962d534076dd20b6f94734a7bed261d5ff424728
David Shupe <shupe@ipac.caltech.edu>
2018-09-24 15:30:53 -0700

    specify minimum Python version to be 3.6

--- setup.py
+++ setup.py
@@ -8,8 +8,14 @@ setup(
     name="hiresprv",
     version="1.0.0",
     author="Mihseh Kong, John Good, BJ Fulton",
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Astronomy'],
     packages=find_packages(),
     data_files=[],
     install_requires=reqs,
+    python_requires='>= 3.6',
     include_package_data=False
 )
